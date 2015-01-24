6f13dc8c7bc3916114d555bfa140ebf86182bf9c
Boombostak <smokejaguar3050@hotmail.com>
2015-01-24 13:37:54 -0700

    The rest of the commit

---
+++ BvW/Assets/_scripts/MouseControl.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseControl : MonoBehaviour {
+
+    RaycastHit hit;
+    public float raycastlength = 500;
+    public bool attackmode = false;
+    public bool movemode = false;
+    public GameObject movecursor;
+    public GameObject attackcursor;
+    public GameObject stopcursor;
+    public GameObject selected_unit;
+    public UnitController uc;
+
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, raycastlength))
+        {
+            Debug.Log(hit.collider.name);
+            if (hit.collider.tag == "Terrain")
+            {
+                if (Input.GetMouseButtonDown(0) && (movemode==true) && (selected_unit!=null))
+                {
+                    GameObject movetarget = Instantiate(movecursor, hit.point, Quaternion.identity) as GameObject;
+                    selected_unit.transform.LookAt(movetarget.transform);
+                    uc.canmove = true;
+                    //movemode = false;
+                }
+
+                if (Input.GetMouseButtonDown(0) && (attackmode==true) && (selected_unit!=null))
+                {
+                    GameObject movetarget = Instantiate(attackcursor, hit.point, Quaternion.identity) as GameObject;
+                    selected_unit.transform.LookAt(movetarget.transform);
+                    
+                    attackmode = false;
+                }
+            }
+
+            if ((hit.collider.tag == "unit") && (movemode ==false) && (attackmode==false))
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    selected_unit = hit.collider.gameObject.transform.parent.gameObject;
+                    uc = selected_unit.GetComponent<UnitController>();
+                }
+                
+            }
+        }
+
+        if (Input.GetButtonDown("move"))
+        {
+            movemode = true;
+        }
+
+        if (Input.GetButtonDown("attack"))
+        {
+            attackmode = true;
+        }
+
+        if (Input.GetButtonDown("stop"))
+        {
+            movemode = false;
+            attackmode = false;
+        }
+        Debug.DrawRay(ray.origin, ray.direction * raycastlength, Color.yellow);
+	
+    }
+
+}
---
+++ BvW/Assets/_scripts/UnitController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitController : MonoBehaviour {
+
+    public float movespeed = 1;
+    public float firerate = 1;
+    public bool canmove = false;
+    public bool canshoot = false;
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+        Shoot();
+	}
+
+    public void Move()
+    {
+        if(canmove==true)
+        {
+            transform.Translate(Vector3.forward * movespeed * Time.deltaTime);
+        }
+        
+    }
+
+    public void Shoot()
+    {
+        canshoot = true;
+    }
+
+    public void Stop()
+    {
+        canmove = false;
+        canshoot = false;
+    }
+}
